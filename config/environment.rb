763528832ed76efb28d3018845870e25fea47e02
mat_aki <matsumura@openskip.org>
2009-05-24 22:22:02 +0900

    use piki_doc in markdown

--- config/environment.rb
+++ config/environment.rb
@@ -17,6 +17,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem "rspec", :lib => "spec"
   config.gem "configatron"
+  config.gem "moro-piki_doc", :lib => "piki_doc"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
